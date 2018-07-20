339ab55049c8237a2aecae7922a8b9b021887be4
John Hutcheson <hutch@theresearchgroup.io>
2018-07-20 19:40:28 -0400

    make delete calls when no more linter errors

--- ReportSuccess.php
+++ ReportSuccess.php
@@ -5,6 +5,7 @@ $build_php_version = $argv[1];
 $linter_bot_token = getenv('LINTER_TOKEN');
 $repo_slug = getenv('TRAVIS_REPO_SLUG');
 $pull_request = getenv('TRAVIS_PULL_REQUEST');
+$linter_github_id = getenv('LINTER_USER_ID');
 
 print "Getting Comments";
 
@@ -15,8 +16,41 @@ $auth = [
 
 $ch = curl_init("https://api.github.com/repos/" . $repo_slug . "/issues/" . $pull_request . "/comments");
 curl_setopt($ch, CURLOPT_HTTPHEADER, $auth);
-curl_setopt($ch, CURLOPT_HEADER, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($ch);
+$serialized_comments = curl_exec($ch);
 
-var_dump($response);
+$comments = json_decode($serialized_comments);
+
+if (!$comments) {
+    print "\nCOULD NOT DESERIALIZE COMMENTS\n";
+}
+
+$linter_comment_ids = [];
+
+// if the response was not an empty array get linters comments from the array
+if (!empty($comments)) {
+    foreach ($comments as $comment) {
+        if ($comment->user->id == $linter_github_id) {
+            $linter_comment_ids[] = $comment->id;
+        }
+    }
+}
+
+if (!empty($linter_comment_ids)) {
+    foreach ($linter_comment_ids as $comment_id) {
+        $handle = curl_init("https://api.github.com/repos/" . $repo_slug . "/issues/" . $pull_request . "/comments/" . $comment_id);
+        curl_setopt($handle, CURLOPT_HTTPHEADER, $auth);
+        curl_setopt($handle, CURLOPT_HEADER, true);
+        curl_setopt($handle, CURLOPT_CUSTOMREQUEST, "DELETE");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($handle);
+
+        $http_code = intval(curl_getinfo($handle, CURLINFO_HTTP_CODE));
+
+        if ($http_code == 204) {
+            print "\nDELETED LINTER COMMENT\n";
+        } else {
+            print "\nUNABLE TO DELETE LINTER COMMENT\n";
+        }
+    }
+}
