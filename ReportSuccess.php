ced5bc4ff678ef5a8fd37e0ef47674cc36589026
John Hutcheson <hutch@theresearchgroup.io>
2018-07-20 18:51:33 -0400

    add visible comment

--- ReportSuccess.php
+++ ReportSuccess.php
@@ -6,6 +6,8 @@ $linter_bot_token = getenv('LINTER_TOKEN');
 $repo_slug = getenv('TRAVIS_REPO_SLUG');
 $pull_request = getenv('TRAVIS_PULL_REQUEST');
 
+print "Getting Comments";
+
 $auth = [
     "Authorization: token $linter_bot_token",
     "User-Agent: jhut89/Mailchimp-API-3.0-PHP (https://github.com/Jhut89/Mailchimp-API-3.0-PHP)"
