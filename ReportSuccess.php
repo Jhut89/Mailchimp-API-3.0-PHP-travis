e043cb00f26110fca30cba0d6a7df9adc1c38668
John Hutcheson <hutch@theresearchgroup.io>
2018-07-20 21:01:44 -0400

    add linter apology and auto label

--- ReportSuccess.php
+++ ReportSuccess.php
@@ -22,7 +22,7 @@ $serialized_comments = curl_exec($ch);
 $comments = json_decode($serialized_comments);
 
 if ($comments === false) {
-    print "\nCOULD NOT DESERIALIZE COMMENTS\n";
+    print "\nCould Not Deserialize Comments\n";
 }
 
 $linter_comment_urls = [];
@@ -35,28 +35,65 @@ if (!empty($comments)) {
         }
     }
 } else {
-    print "\nNO COMMENTS WERE FOUND\n";
+    print "\nNo Comments Were Found\n";
 }
 
 if (!empty($linter_comment_urls)) {
-    print "\nDELETING LINTER COMMENTS...\n";
+    print "\nDeleting Linter Comments...\n";
     foreach ($linter_comment_urls as $comment_url) {
         $handle = curl_init($comment_url);
         print "$comment_url";
         curl_setopt($handle, CURLOPT_HTTPHEADER, $auth);
         curl_setopt($handle, CURLOPT_HEADER, true);
         curl_setopt($handle, CURLOPT_CUSTOMREQUEST, "DELETE");
-        //curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
         $response = curl_exec($handle);
 
         $http_code = intval(curl_getinfo($handle, CURLINFO_HTTP_CODE));
 
         if ($http_code == 204) {
-            print "\nDELETED LINTER COMMENT\n";
+            print "\nDeleted Linter Comment\n";
         } else {
-            print "\nUNABLE TO DELETE LINTER COMMENT\n";
+            print "\nUnable To Delete Comment\n";
         }
     }
 } else {
-    print "\nCOULD NOT FIND ANY LINTER COMMENTS\n";
+    print "\nCould Not Find Any Linter Comments\n";
+}
+
+$apology = "The **Lint Wizard** redacts anything he may have said before! :sparkles::sparkles::sparkles:";
+$apology .= "\n\n`Lint Wizard Approved` :heavy_check_mark: ";
+
+$serialized_apology = json_encode(["body" => $apology]);
+$apology_handle = curl_init("https://api.github.com/repos/" . $repo_slug . "/issues/" . $pull_request . "/comments");
+curl_setopt($apology_handle, CURLOPT_HTTPHEADER, $auth);
+curl_setopt($apology_handle, CURLOPT_HEADER, true);
+curl_setopt($apology_handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($apology_handle, CURLOPT_POST, true);
+curl_setopt($apology_handle, CURLOPT_POSTFIELDS, $serialized_apology);
+$response = curl_exec($apology_handle);
+
+$http_code = intval(curl_getinfo($apology_handle, CURLINFO_HTTP_CODE));
+if ($http_code > 199 && $http_code < 300) {
+    print "\nLint Wizard Apologized...\n";
+} else {
+    print "\nUnable to Apologize\n";
+}
+
+$serialized_label = json_encode(["Linter Approved"]);
+
+$label_handle = curl_init("https://api.github.com/repos/" . $repo_slug . "/issues/" . $pull_request . "/labels");
+curl_setopt($label_handle, CURLOPT_HTTPHEADER, $auth);
+curl_setopt($label_handle, CURLOPT_HEADER, true);
+curl_setopt($label_handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($label_handle, CURLOPT_POST, true);
+curl_setopt($label_handle, CURLOPT_POSTFIELDS, $serialized_label);
+$response = curl_exec($label_handle);
+
+$http_code = intval(curl_getinfo($label_handle, CURLINFO_HTTP_CODE));
+
+if ($http_code > 199 && $http_code < 300) {
+    print "\nApplied Linter Label...\n";
+} else {
+    print "\nUnable to Apply Linter Label\n";
 }
